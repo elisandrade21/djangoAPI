67caf7c5dee90aa97db349fcc689c1a298acc213
Elisa Andrade <elisandradecc@gmail.com>
2020-12-14 15:33:35 -0300

    Adicionando os models

--- notifications/models.py
+++ notifications/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Message(models.Model):
+    id = models.AutoField(primary_key=True)
+    title = models.CharField(max_length=100)
+    body = models.TextField()
+    name_receiver = models.CharField(max_length=150)
+    receiver = models.CharField(max_length=150)
+
+    def __str__(self):
+        return self.title
+
+class Notification(models.Model):
+    id = models.AutoField(primary_key=True)
+    send_data = models.DateTimeField()
+    notification_type = models.CharField(max_length=50)
+    send_status = models.BooleanField(default=False)
+    id_message = models.ForeignKey(Message, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return  self.notification_type
+
