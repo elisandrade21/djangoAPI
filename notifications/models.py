dc99e7e652085627f5dd8a15454ed1f569ee5009
Elisa Andrade <elisandradecc@gmail.com>
2020-12-14 15:40:06 -0300

    Modificando o arquivo Models

--- notifications/models.py
+++ notifications/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
-class Message(models.Model):
+class Messages(models.Model):
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100)
     body = models.TextField()
@@ -10,13 +10,18 @@ class Message(models.Model):
     def __str__(self):
         return self.title
 
-class Notification(models.Model):
+    class Meta:
+        verbose_name_plural = "Messages"
+
+class Notifications(models.Model):
     id = models.AutoField(primary_key=True)
     send_data = models.DateTimeField()
     notification_type = models.CharField(max_length=50)
     send_status = models.BooleanField(default=False)
-    id_message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    id_message = models.ForeignKey(Messages, on_delete=models.CASCADE)
 
     def __str__(self):
         return  self.notification_type
 
+    class Meta:
+        verbose_name_plural = "Notifications"
