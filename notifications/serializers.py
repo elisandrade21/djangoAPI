30850d4e621efe30bd52ff5d8d2d5a83c4b62308
Elisa Andrade <elisandradecc@gmail.com>
2020-12-15 15:01:22 -0300

    Adicionando os campos no arquivo serializers

--- notifications/serializers.py
+++ notifications/serializers.py
@@ -3,6 +3,18 @@ from .models import Notification
 
 class NotificationSerializer(serializers.ModelSerializer):
     class Meta:
+        extra_kwargs = {
+            'nome_receiver' : {'write_only': True},
+            'receiver' : {'write_only': True}
+        }
         model: Notification
-        fields: '__all__'
+        fields = (
+            'id',
+            'send_data',
+            'comunication_type',
+            'send_status',
+            'title',
+            'body'
+
+        )
 
