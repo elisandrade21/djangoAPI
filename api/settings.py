76913a325115586ccc3f0dd77b8f96df3c8317c5
Elisa Andrade <elisandradecc@gmail.com>
2020-12-15 13:25:21 -0300

    Ajustando o settings

--- api/settings.py
+++ api/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'notifications',
     'django_filters',
+    'notifications',
 
 ]
 
