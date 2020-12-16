5427d550c8de245708247300c6c545d668d80c2e
Elisa Andrade <elisandradecc@gmail.com>
2020-12-16 13:35:10 -0300

    Alterando as urls.py

--- notifications/urls.py
+++ notifications/urls.py
@@ -3,10 +3,8 @@ from . views import NotificationAPIView
 
 
 urlpatterns = [
-   #path('notifications/<int:pk>', NotificationAPIView.as_view(),name='get_test'),
-    path('notifications/<str:status>', NotificationAPIView.as_view(),name='status'),
+   
     path('notifications/', NotificationAPIView.as_view(),name='notifications'),
     path('notifications/<int:pk>/', NotificationAPIView.as_view(),name='delete')
 
 ]
-
