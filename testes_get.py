cca4ad591b0caab2086f6f318d67885b1f50641a
Elisa Andrade <elisandradecc@gmail.com>
2020-12-17 16:15:35 -0300

    Atualizando o arquivo testes_get

--- testes_get.py
+++ testes_get.py
@@ -1,17 +1,15 @@
 import requests
-"""
 # Testando uma notificação pelo ID 7 
 
 notification = requests.get('http://localhost:8000/api/v1/notifications/7/')
 print(notification.json())
 
+
 # Testando se o endpoint está correto 
 assert notification.status_code == 200
 print(notification.status_code)
-"""
 
 
-"""
 # Testando uma notificação pelo Status Error  
 
 notification_status_error = requests.get('http://localhost:8000/api/v1/notifications/error/')
@@ -20,9 +18,8 @@ print(notification_status_error.json())
 # Testando se o endpoint de listar pelo status = Error está correto 
 assert notification_status_error.status_code == 200
 print(notification_status_error.status_code)
-"""
 
-"""
+
 # Testando uma notificação pelo Status Sent  
 
 notification_status_sent = requests.get('http://localhost:8000/api/v1/notifications/sent/')
@@ -31,7 +28,6 @@ print(notification_status_sent.json())
 # Testando se o endpoint de listar pelo status = Sent está correto 
 assert notification_status_sent.status_code == 200
 print(notification_status_sent.status_code)
-"""
 
 
 # Testando uma notificação pelo Status Waiting
@@ -43,3 +39,6 @@ print(notification_status_waiting.json())
 assert notification_status_waiting.status_code == 200
 print(notification_status_waiting.status_code)
 
+
+# Testando se a notificação com ID 7 
+assert notification.json()['title'] == 'Compra Efetuada'
