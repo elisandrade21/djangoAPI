a7c9b1feff6b1c0606ac493c979a86de4e5e30f0
Elisa Andrade <elisandradecc@gmail.com>
2020-12-16 21:47:31 -0300

    Alterando o urls

--- notifications/urls.py
+++ notifications/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from . views import NotificationAPIView
+from . views import get_notifications_by_status
+
 
 
 urlpatterns = [
+    path('notifications/<str:status>/', get_notifications_by_status, name='pepe'),
     path('notifications/', NotificationAPIView.as_view(),name='notifications'),
     path('notifications/<int:pk>/',NotificationAPIView.as_view(), name='notification')
 ]
